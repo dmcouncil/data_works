6a4c3c9bec8d10076833dd3a90f466817d1cd1ce
Luke Inglis <ld.inglis@gmail.com>
2016-09-12 16:02:41 -0400

    Add spec code for current state

--- spec/relationships_spec.rb
+++ spec/relationships_spec.rb
@@ -1,107 +1,18 @@
 require_relative "helper/data_works_spec_helper"
 
-describe DataWorks::Relationships do
-  describe "#necessary_parents_for" do
-    describe "pet" do
-      it "returns an empty collection" do
-        expect(DataWorks::Relationships.necessary_parents_for(:pet)).to be_empty
-      end
-    end
+describe 'DataWorks#set_restriction' do
+  let!(:data) { TheDataWorks.new }
 
-    describe "pet_food" do
-      it "returns an empty collection" do
-        expect(DataWorks::Relationships.necessary_parents_for(:pet_food)).to be_empty
-      end
-    end
+  describe 'setting a restriction using the metheod/registry option'do
+    let!(:darth_cuddles) { data.add_pet }
+    let!(:kittylo_ren) { data.add_pet }
 
-    describe "agency" do
-      it "returns an empty collection" do
-        expect(DataWorks::Relationships.necessary_parents_for(:agency)).to be_empty
-      end
-    end
-
-    describe "toy" do
-      it "returns a collection with a single parent object " do
-        expect(DataWorks::Relationships.necessary_parents_for(:toy).count).to eq 1
-      end
-      it "returns a parent object with #association_name => pet" do
-        parents = DataWorks::Relationships.necessary_parents_for(:toy)
-        expect(parents.first.association_name).to eq(:pet)
-      end
-      it "returns a parent object with #model_name => pet" do
-        parents = DataWorks::Relationships.necessary_parents_for(:toy)
-        expect(parents.first.model_name).to eq(:pet)
-      end
-    end
-
-    describe "tag" do
-      it "returns a collection with a single parent object " do
-        expect(DataWorks::Relationships.necessary_parents_for(:tag).count).to eq 1
-      end
-      it "returns a parent object with #association_name => pet" do
-        parents = DataWorks::Relationships.necessary_parents_for(:tag)
-        expect(parents.first.association_name).to eq(:pet)
-      end
-      it "returns a parent object with #model_name => pet" do
-        parents = DataWorks::Relationships.necessary_parents_for(:tag)
-        expect(parents.first.model_name).to eq(:pet)
-      end
-    end
-
-    describe "pet_profile" do
-      it "returns a collection with a single parent object " do
-        expect(DataWorks::Relationships.necessary_parents_for(:pet_profile).count).to eq 1
-      end
-      it "returns a parent object with #association_name => pet" do
-        parents = DataWorks::Relationships.necessary_parents_for(:pet_profile)
-        expect(parents.first.association_name).to eq(:pet)
-      end
-      it "returns a parent object with #model_name => pet" do
-        parents = DataWorks::Relationships.necessary_parents_for(:pet_profile)
-        expect(parents.first.model_name).to eq(:pet)
-      end
-    end
-
-    describe "pet_sitter" do
-      it "returns a collection with a single parent object " do
-        expect(DataWorks::Relationships.necessary_parents_for(:pet_sitter).count).to eq 1
-      end
-      it "returns a parent object with #association_name => agency" do
-        parents = DataWorks::Relationships.necessary_parents_for(:pet_sitter)
-        expect(parents.first.association_name).to eq(:agency)
-      end
-      it "returns a parent object with #model_name => agency" do
-        parents = DataWorks::Relationships.necessary_parents_for(:pet_sitter)
-        expect(parents.first.model_name).to eq(:agency)
-      end
-    end
-
-    describe "address" do
-      it "returns a collection with a single parent object " do
-        expect(DataWorks::Relationships.necessary_parents_for(:address).count).to eq 1
-      end
-      it "returns a parent object with #association_name => pet_profile" do
-        parents = DataWorks::Relationships.necessary_parents_for(:address)
-        expect(parents.first.association_name).to eq(:pet_profile)
-      end
-      it "returns a parent object with #model_name => pet_profile" do
-        parents = DataWorks::Relationships.necessary_parents_for(:address)
-        expect(parents.first.model_name).to eq(:pet_profile)
-      end
-    end
-
-    describe "pet_sitting_patronage" do
-      it "returns a collection with two parent objects" do
-        expect(DataWorks::Relationships.necessary_parents_for(:pet_sitting_patronage).count).to eq 2
-      end
-      it "returns an array of parent objects with #association_names => pet_sitter, pet" do
-        parents = DataWorks::Relationships.necessary_parents_for(:pet_sitting_patronage)
-        expect(parents.map(&:association_name)).to contain_exactly(:pet_sitter, :pet)
-      end
-      it "returns an array of parent objects with #model_names => pet_sitter, pet" do
-        parents = DataWorks::Relationships.necessary_parents_for(:pet_sitting_patronage)
-        expect(parents.map(&:model_name)).to contain_exactly(:pet_sitter, :pet)
-      end
+    it 'restricts the parent record for new children to the given object' do
+      death_star = data.add_toy
+      data.set_restriction(for_model: :pet, to: kittylo_ren)
+	  star_killer = data.add_toy
+      expect(death_star.pet).to eq darth_cuddles
+      expect(star_killer.pet).to eq kittylo_ren
     end
   end
 end
