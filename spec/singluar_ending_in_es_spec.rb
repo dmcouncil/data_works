0ef773e38daf85da27de8932483dc16ef09c4ac2
Luke Inglis <ld.inglis@gmail.com>
2016-09-12 16:30:00 -0400

    Add clarifying comments to spec

--- spec/singluar_ending_in_es_spec.rb
+++ spec/singluar_ending_in_es_spec.rb
@@ -8,10 +8,10 @@ describe TheDataWorks do
       it 'creates a new Address record' do
         expect { data.add_address }.to change(Address, :count).by(1)
       end
-      it 'creates a new PetProfile record' do
+      it 'creates a new PetProfile record' do # require parent for Address
         expect { data.add_address }.to change(PetProfile, :count).by(1)
       end
-      it 'creates a new Pet record' do
+      it 'creates a new Pet record' do # required parent for PetProfile
         expect { data.add_address }.to change(Pet, :count).by(1)
       end
     end
@@ -20,10 +20,10 @@ describe TheDataWorks do
       it 'creates three new Address records' do
         expect { data.add_addresses(3) }.to change(Address, :count).by(3)
       end
-      it 'creates a new PetProfile record' do
+      it 'creates a new PetProfile record' do # require parent for Address
         expect { data.add_addresses(3) }.to change(PetProfile, :count).by(1)
       end
-      it 'creates a new Pet record' do
+      it 'creates a new Pet record' do# required parent for PetProfile
         expect { data.add_addresss(3) }.to change(Pet, :count).by(1)
       end
     end
